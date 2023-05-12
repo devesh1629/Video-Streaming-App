5b4a5f905659897bc7a475875c2d0cfdaa60c2df
Devesh Agarwal <devesagr@xiaomi.com>
2023-05-12 15:13:06 +0530

    Adding upload feature

--- client/src/Home.js
+++ client/src/Home.js
@@ -8,7 +8,10 @@ export default class Home extends Component {
   constructor() {
     super();
     this.state = {
-      videos: []
+      videos: [],
+      source: '',
+      file: '',
+      fileName: ''
     };
   }
 
@@ -22,12 +25,40 @@ export default class Home extends Component {
     }
   }
 
+  handleChange = (event) => {
+    const video = event.target.files[0];
+    if(video !== undefined) {
+      const url = URL.createObjectURL(video);
+      this.setState({source: url});
+      this.setState({file: event.target.files[0]});
+      this.setState({fileName: event.target.files[0].name})
+    }
+  };
+
+  uploadFile = async (e) => {
+    const formData = new FormData();
+    formData.append("baseUrl", this.state.source);
+    formData.append("file", this.state.file);
+    formData.append("fileName", this.state.fileName);
+    fetch('http://localhost:4000/upload', {
+      method: 'POST',
+      body: formData,
+      redirect: 'follow'
+    }).then((data) => {
+      alert("Video upload successful")
+    })
+    this.setState({source:''});
+    this.setState({file:''});
+    this.setState({fileName:''});
+  };
+
   render() {
     return (
       <div className="App-header">
         <Header />
-          <div className="row">
-            {this.state.videos.map(video =>
+        <div className="row">
+          {
+            this.state.videos.map(video =>
               <div className="col-md-4" key={video.id}>
                 <Link to={`/player/${video.id}`}>
                   <div className="card border-0">
@@ -38,8 +69,19 @@ export default class Home extends Component {
                   </div>
                 </Link>
               </div>
-            )}
-          <Footer />
+          )}
+            <Footer />
+        </div>
+        
+        <div className="VideoInput">
+          <input
+            // ref={inputRef}
+            className="VideoInput_input"
+            type="file"
+            onChange={this.handleChange}
+            accept=".mov,.mp4"
+          />
+          {this.state.source && (<button onClick={this.uploadFile}>Upload Video</button>)}
         </div>
       </div>
     )
